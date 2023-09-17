9ed1588850e5334000056ced3f297995c1bffe25
Lvova Alina <lvova.aline@gmail.com>
2023-09-17 21:39:55 +0400

    feature: add frontend

---
+++ mts-tarrifs-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TariffsParser from "./TariffsParser";
+
+function App() {
+  return (
+    <div className="App">
+      <TariffsParser />
+    </div>
+  );
+}
+
+export default App;
